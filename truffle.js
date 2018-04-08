3f30f68d357b5e878fef013b57cf17d55b0946e7
finley <573108440@qq.com>
2018-04-08 18:53:38 +0800

    ef
    test

--- truffle.js
+++ truffle.js
@@ -1,9 +1,9 @@
 module.exports = {
   networks: {
     development: {
-      host: "localhost",
+      host: "127.0.0.1",
       port: 8545,
-      network_id: "*" // Match any network id
+      network_id: "93318" // Match any network id
     }
   }
 };
