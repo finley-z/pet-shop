cbbfa350b693c9d8f23abf1ff3eadc5d3b995918
finley <573108440@qq.com>
2018-04-12 18:08:54 +0800

    test

--- truffle.js
+++ truffle.js
@@ -5,7 +5,7 @@ module.exports = {
     development: {
       host: "localhost",
       port: 8545,
-      network_id: "99661" // Match any network id
+      network_id: "666" // Match any network id
     }
   }
 };
